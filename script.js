06c2eee8caaf10eec34ae72b7243b02c62f57628
L3sley-cpu <wscorp21.gmail.com>
2026-02-19 16:11:35 -0300

    adicionei mais uma mecanica de caso haja algum erro

--- script.js
+++ script.js
@@ -1,9 +1,13 @@
-function generateNumber(){
+function generateNumber() {
 
-    const min = Math.ceil (document.querySelector('.input1').value);
+    const min = Math.ceil(document.querySelector('.input1').value);
     const max = Math.floor(document.querySelector('.input2').value);
 
     const result = Math.floor(Math.random() * (max - min + 1)) + min;
 
-    document.querySelector('.numero').innerHTML = result;   
+    document.querySelector('.numero').innerHTML = result;
+
+    if (isNaN(result) || min == max) {
+        document.querySelector('.numero').innerHTML = 'Erro';
+    }
 }
