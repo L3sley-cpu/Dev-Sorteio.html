0b1c4b00f9504db2a3fb438040edd6024d1d0802
L3sley-cpu <wscorp21.gmail.com>
2026-02-19 16:38:47 -0300

    Botei prea quebrar

--- script.js
+++ script.js
@@ -8,10 +8,22 @@ function generateNumber() {
     document.querySelector('.numero').innerHTML = result;
 
     if (isNaN(result) || min == max) {
-        document.querySelector('.numero').innerHTML = 'Erro';
+        document.querySelector('.numero').innerHTML = 'Digite um numero valido';
+        document.querySelector('.numero').style.fontSize = '20px';
+        }
+
+    else{
+        document.querySelector('.numero').innerHTML = result;
+        document.querySelector('.numero').style.fontSize = '50px';
     }
 
-    else if ((result) || min < max) {
-        document.querySelector('.numero').innerHTML = 'Erro';
+    if (min > max) {
+        document.querySelector('.numero').innerHTML = 'O primeiro numero deve ser menor que o segundo';
+        document.querySelector('.numero').style.fontSize = '20px';
+    }
+
+    else if ((result) || min < max ) {
+        document.querySelector('.numero').innerHTML = result;
+        document.querySelector('.numero').style.fontSize = '50px';
     }
 }
