902e670442922fb0028a59afdfff1d669edccd81
L3sley-cpu <wscorp21.gmail.com>
2026-02-19 16:47:42 -0300

    mudei nada

--- script.js
+++ script.js
@@ -16,14 +16,8 @@ function generateNumber() {
         document.querySelector('.numero').innerHTML = result;
         document.querySelector('.numero').style.fontSize = '50px';
     }
-
-    if (min > max) {
-        document.querySelector('.numero').innerHTML = 'O primeiro numero deve ser menor que o segundo';
-        document.querySelector('.numero').style.fontSize = '20px';
-    }
-
-    else if ((result) || min < max ) {
-        document.querySelector('.numero').innerHTML = result;
-        document.querySelector('.numero').style.fontSize = '50px';
-    }
+    
 }
+
+
+
